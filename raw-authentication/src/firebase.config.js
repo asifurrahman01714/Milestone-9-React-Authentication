2bf1e4bc64bdc2a8441d1652fede4d3ee76585c4
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-01-29 13:23:55 +0600

    Added firebase config file

---
+++ raw-authentication/src/firebase.config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyARRWu1ADmyfM-mjt2Ywy33dzeaOjBcSiU",
+  authDomain: "ema-john-simple-d8e9e.firebaseapp.com",
+  projectId: "ema-john-simple-d8e9e",
+  storageBucket: "ema-john-simple-d8e9e.appspot.com",
+  messagingSenderId: "344104381732",
+  appId: "1:344104381732:web:c7036e1c15cfff30f297b1",
+  measurementId: "G-0QH3L52B3Y"
+};
